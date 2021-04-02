83efafaa5ce201f214528ac9ba88304589ad414e
nicklavy <79679948+nicklavy@users.noreply.github.com>
2021-04-02 10:31:50 -0500

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,10 +2,10 @@
 module.exports = {
   // this user MUST have full access to all the room accounts
   'exchange' : {
-    'username'  : process.env.USERNAME || 'SVCACCT_EMAIL@DOMAIN.COM',
-    'password'  : process.env.PASSWORD || 'PASSWORD',
+    'username'  : process.env.USERNAME || 'svc_OneLanRCM@horizonpharma.onmicrosoft.com',
+    'password'  : process.env.PASSWORD || 'G@dhOCTPqJ2^',
     'uri'       : 'https://outlook.office365.com/EWS/Exchange.asmx'
   },
   // Ex: CONTOSO.COM, Contoso.com, Contoso.co.uk, etc.
-  'domain' : process.env.DOMAIN || 'DOMAIN.COM'
+  'domain' : process.env.DOMAIN || 'horizonpharma.com'
 };
